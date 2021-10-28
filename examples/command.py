41a38456426d8580283b8f3fc2c46ea6c1da3b09
5IGI0 <5IGI0@protonmail.com>
2021-10-28 08:16:00 +0200

    Initial commit

---
+++ examples/command.py
@@ -0,0 +1,30 @@
+from asynctelegram import Bot
+from os import environ
+
+class MyBot(Bot):
+    async def on_login(self):
+        print(f"logged in as {self.user.username}")
+
+bot = MyBot()
+
+@bot.command(hide=True)
+async def start(ctx, args):
+    cliched_sentences = {
+        "fr": "hon hon hon, j'aime les baguettes",
+        "ru": "vodka!"
+    }
+
+    lang = ctx.author.language_code
+
+    await ctx.send(cliched_sentences[lang] if cliched_sentences.get(lang) is not None else "hello")
+
+@bot.command(name="ping", description="ping the bot")
+async def ping(ctx, args):
+    await ctx.send("pong")
+
+@bot.command(hide=True)
+async def shutdown(ctx, args):
+    await ctx.send("bye")
+    bot.stop()
+
+bot.start(environ["TOKEN"])
